d89feda0bf674dc14601263af880e3afa38ecc16
kgrigoruk <kolyabamberg@gmail.com>
2018-04-27 19:48:31 +0300

    done good month view

--- front/appVacation/src/app/calendar/calendar.component.ts
+++ front/appVacation/src/app/calendar/calendar.component.ts
@@ -8,7 +8,9 @@ import {Component, OnInit} from "@angular/core";
 
 export class CalendarComponent implements OnInit {
 
-    months: any[] = new Array(12);
+    months: any[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    year: number = 2018;
+
     constructor() {
     }
 
--- front/appVacation/src/app/calendar/month/month.component.ts
+++ front/appVacation/src/app/calendar/month/month.component.ts
@@ -9,9 +9,36 @@ import {Component, Input, OnInit} from "@angular/core";
 
 export class MonthComponent implements OnInit {
     @Input() month: number;
+    @Input() year:number;
+
+    weeks: any[] = [1,2,3,4,5];
     constructor() {
     }
 
     ngOnInit() {
+    }
+
+    _getWidth(week: number) {
+      if (week == 1) {
+        let day = new Date(this.year, this.month - 1,1).getDay();
+        day = day == 0 ? 6: day - 1;
+        return 13.25 * (7 - day) + '%';
+      }
+
+      if (week == 5) {
+        let day= new Date(this.year, this.month, 0).getDay();
+        day = day == 0 ? 7: day;
+        return 13.25 * (day) +'%';
+      }
+    }
+
+    _getOffset(week:number) {
+      if(week == 1) {
+       let day = new Date(this.year, this.month - 1,1).getDay();
+        day = day == 0 ? 6: day - 1;
+        if (day != 0) {
+          return 96 - 13.25 * (7 - day) + '%';
+        }
+      }
     }
 }
